51cd6666fc6c80b19a488371385ff5ab3227bc08
kimhm0728 <kimhm0728@naver.com>
2023-11-09 22:14:56 +0900

    feat: 주문 메뉴 입력받는 기능 구현

--- src/main/kotlin/christmas/view/input/InputValidator.kt
+++ src/main/kotlin/christmas/view/input/InputValidator.kt
@@ -3,18 +3,34 @@ package christmas.view.input
 import christmas.constants.Exception
 
 object InputValidator {
-    fun checkVisitDate(inputDate: String) {
+    private val orderMenuRegex = "^[가-힣]+-\\d+$".toRegex()
+
+    fun checkVisitDate(inputDate: String) =
         with(Exception.VISIT_DATE.toString()) {
             validateEmpty(inputDate, this)
             validateInt(inputDate, this)
         }
-    }
 
-    private fun validateEmpty(input: String, errorMsg: String) {
+    fun checkOrderMenu(inputOrder: String) =
+        with(Exception.VISIT_ORDER.toString()) {
+            inputOrder.toOrderMenus().forEach { menus ->
+                validateEmpty(menus, this)
+                validateRegex(menus, this)
+            }
+        }
+
+    private fun validateEmpty(input: String, errorMsg: String) =
         require(input.isNotEmpty()) { errorMsg }
-    }
 
-    private fun validateInt(input: String, errorMsg: String) {
+    private fun validateInt(input: String, errorMsg: String) =
         require(input.toIntOrNull() != null) { errorMsg }
-    }
+
+    private fun validateRegex(input: String, errorMsg: String) =
+        require(orderMenuRegex.matches(input)) { errorMsg }
+
+    private fun String.toOrderMenus() = this.split(",")
+
+    private fun String.toMenuAndCount() = this.split("-")
+
+
 }
