24b72ff5a722320cd7a06682da5bee951e09de45
kimhm0728 <kimhm0728@naver.com>
2023-11-11 23:23:36 +0900

    refactor: 상수 용도에 맞게 분리

--- src/main/kotlin/christmas/constants/Constants.kt
+++ src/main/kotlin/christmas/constants/Constants.kt
@@ -1,11 +1,18 @@
 package christmas.constants
 
 import christmas.constants.menu.Drink
+import christmas.model.Price
 
 enum class Constants(val value: Int) {
     DATE_START(1),
     DATE_END(31),
-    ORDER_SIZE(20),
-    PRESENT_PRICE(Drink.CHAMPAGNE.price),
-    EVENT_STANDARD(10000)
+    ORDER_MAX_SIZE(20),
+    CHRISTMAS_DATE(25),
+}
+
+enum class PriceConstants(val price: Price) {
+    PRESENT_PRICE(Price(Drink.CHAMPAGNE.price)),
+    NO_PRESENT_PRICE(Price()),
+    EVENT_STANDARD(Price(10000)),
+    PRESENT_STANDARD(Price(120000)),
 }
--- src/main/kotlin/christmas/constants/Exception.kt
+++ src/main/kotlin/christmas/constants/Exception.kt
@@ -1,14 +1,15 @@
 package christmas.constants
 
 enum class Exception(private val msg: String) {
-    VISIT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    ORDER_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    ORDER_MENU_DRINK_ONLY("음료만 주문할 수 없습니다. 다시 입력해 주세요."),
-    ORDER_MENU_MAX_SIZE("최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
+    VISIT_DATE("유효하지 않은 날짜입니다."),
+    ORDER_MENU("유효하지 않은 주문입니다."),
+    ORDER_MENU_DRINK_ONLY("음료만 주문할 수 없습니다. "),
+    ORDER_MENU_MAX_SIZE("최대 20개까지만 주문할 수 있습니다.");
 
-    override fun toString() = "$ERROR_MSG $msg"
+    override fun toString() = "$ERROR_MSG $msg $RE_INPUT"
 
     companion object {
         private const val ERROR_MSG = "[ERROR]"
+        private const val RE_INPUT = "다시 입력해 주세요."
     }
 }
