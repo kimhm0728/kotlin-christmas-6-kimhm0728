d9a521863b2ffdda38ddbc59a7851d150cdd0cd6
kimhm0728 <kimhm0728@naver.com>
2023-11-12 02:04:32 +0900

    style: 예외 문자열 오타 수정

--- src/main/kotlin/christmas/constants/Exception.kt
+++ src/main/kotlin/christmas/constants/Exception.kt
@@ -3,7 +3,7 @@ package christmas.constants
 enum class Exception(private val msg: String) {
     VISIT_DATE("유효하지 않은 날짜입니다."),
     ORDER_MENU("유효하지 않은 주문입니다."),
-    ORDER_MENU_DRINK_ONLY("음료만 주문할 수 없습니다. "),
+    ORDER_MENU_DRINK_ONLY("음료만 주문할 수 없습니다."),
     ORDER_MENU_MAX_SIZE("최대 20개까지만 주문할 수 있습니다.");
 
     override fun toString() = "$ERROR_MSG $msg $RE_INPUT"
