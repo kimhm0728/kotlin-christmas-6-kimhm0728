dd7cb4cd0236e96dcb9af6d2d963be8c0b213e76
kimhm0728 <kimhm0728@naver.com>
2023-11-11 23:23:14 +0900

    refactor: 상수값 Int를 래퍼 클래스로 변경

--- src/main/kotlin/christmas/constants/Badge.kt
+++ src/main/kotlin/christmas/constants/Badge.kt
@@ -1,10 +1,12 @@
 package christmas.constants
 
-enum class Badge(private val badgeName: String, val minimumPrice: Int) {
-    SANTA("산타", 20000),
-    TREE("트리", 10000),
-    STAR("별", 5000),
-    NO_BADGE("없음", 0);
+import christmas.model.Price
+
+enum class Badge(private val badgeName: String, val minimumPrice: Price) {
+    SANTA("산타", Price(20000)),
+    TREE("트리", Price(10000)),
+    STAR("별", Price(5000)),
+    NO_BADGE("없음", Price(0));
 
     override fun toString() = badgeName
 }
