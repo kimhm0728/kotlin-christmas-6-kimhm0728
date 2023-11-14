4b608efb134a094a3118fe3c21459e776b8e841d
kimhm0728 <kimhm0728@naver.com>
2023-11-14 22:13:56 +0900

    style: 출력 형식 변경

--- src/main/kotlin/christmas/view/output/OutputView.kt
+++ src/main/kotlin/christmas/view/output/OutputView.kt
@@ -14,7 +14,7 @@ object OutputView {
     fun printStartGreeting() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
 
     fun printEventPreviewInfo() {
-        println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
+        println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
         lineBreak()
     }
 
